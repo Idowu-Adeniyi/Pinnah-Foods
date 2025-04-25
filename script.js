259cd66840a9787b9ce3a2dc6e1459d0218ec080
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-04-25 12:48:59 -0400

    added media query for tablet and mobile

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//alert("Hello World!");
+const mobileMenuOpen = document.querySelector(".fa-bars");
+const mobileMenuOpenBtn = document.getElementById("openMenu");
+const showNav = document.getElementById("nav-ul-show");
+
+const mobileMenuClose = document.querySelector(".fa-xmark");
+const mobileMenuCloseBtn = document.getElementById("closeMenu");
+const hideNav = document.getElementById("nav-ul-show");
+
+//const contactBtn = document.querySelector(".contact-cta");
+
+function openBtn() {
+  mobileMenuOpen.addEventListener("click", () => {
+    showNav.style.display = "block";
+    mobileMenuOpen.style.display = "none";
+    mobileMenuClose.style.display = "block";
+  });
+}
+
+function closeBtn() {
+  mobileMenuClose.addEventListener("click", () => {
+    showNav.style.display = "none";
+    mobileMenuClose.style.display = "none";
+    mobileMenuOpen.style.display = "block";
+  });
+}
+
+if (mobileMenuOpen) {
+  openBtn();
+}
+if (mobileMenuClose) {
+  closeBtn();
+}
